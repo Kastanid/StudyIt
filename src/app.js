b1e41d66972b1e180d109b0b92149991412f7925
Siim Erik <siimpotsepp14@gmail.com>
2018-02-15 11:22:27 +0200

    css juures

--- src/app.js
+++ src/app.js
@@ -1,5 +1,11 @@
 export class App {
   constructor() {
-    this.message = 'Tere World!';
+    this.message = 'Kastanid!';
+
+    this.styleString = `color: gray;
+    background-color: rgb(40,40,40);
+    width: 100%;
+    height: 60px;
+    `;
   }
 }
