b3f9484426650cb649e8b5d6667d941fa44219f2
T1mo <kasktimo@gmail.com>
2018-02-26 20:09:17 +0200

    Testin

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ export class App {
     this.message = 'Kastanid!';
 
     this.styleString = `color: gray;
-    background-color: rgb(30,40,40);
+    background-color: rgb(35,40,40);
     width: 110%;
     height: 60px;
     `;
