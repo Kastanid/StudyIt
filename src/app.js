3482265570b4566b55be2bb867d4cc15d8959624
Siim Erik <siimpotsepp14@gmail.com>
2018-02-27 10:36:51 +0200

    projekti fix

--- src/app.js
+++ src/app.js
@@ -1,14 +1,4 @@
 export class App {
-<<<<<<< HEAD
-  constructor() {
-    this.message = 'Kastanid!';
-
-    this.styleString = `color: gray;
-    background-color: rgb(30,40,40);
-    width: 110%;
-    height: 60px;
-    ;
-=======
   configureRouter(config, router){
     config.title = 'StudyIt';
     config.map([
@@ -19,6 +9,5 @@ export class App {
       { route: '/arvustuseVorm', moduleId: 'arvustuseVorm', name:'arvustuseVorm' }
     ]);
     this.router = router;
->>>>>>> 3c72e00bc6c35b0cc7e0910de0614e08abf83a63
   }
 }
