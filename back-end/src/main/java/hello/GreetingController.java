958d806499dee2d402bd9093cc6d515db6a947c7
Siim Erik <siimpotsepp14@gmail.com>
2018-03-05 16:08:47 +0200

    Uus projekt

--- back-end/src/main/java/hello/GreetingController.java
+++ back-end/src/main/java/hello/GreetingController.java
@@ -15,7 +15,7 @@ public class GreetingController {
 
     @CrossOrigin(origins = "http://localhost:9000")
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="Maailm") String name) {
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
