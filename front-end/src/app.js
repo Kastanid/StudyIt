30611e31c986c872e92aa76c3142d54325413234
Siim Erik <siimpotsepp14@gmail.com>
2018-03-05 11:31:41 +0200

    Loodame et töötab

---
+++ front-end/src/app.js
@@ -0,0 +1,13 @@
+export class App {
+  configureRouter(config, router){
+    config.title = 'StudyIt';
+    config.map([
+      { route: '',              moduleId: 'mainPage',   title: 'Main'},
+      { route: '/mainPage',      moduleId: 'mainPage', name:'mainPage' },
+      { route: '/kkk',           moduleId: 'kkk', name:'kkk' },
+      { route: '/kontaktid',     moduleId: 'kontaktid', name:'kontaktid' },
+      { route: '/arvustuseVorm', moduleId: 'arvustuseVorm', name:'arvustuseVorm' }
+    ]);
+    this.router = router;
+  }
+}
