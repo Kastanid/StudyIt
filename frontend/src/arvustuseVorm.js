7b0d37e22f68345527a922b510d6c518c36472d5
Timo <kasktimo@gmail.com>
2018-04-04 12:36:03 +0300

    Arvustuse lisamine andmebaasi ja sealt andmete kuvamine algselt ilma vorminguta

--- frontend/src/arvustuseVorm.js
+++ frontend/src/arvustuseVorm.js
@@ -1,4 +1,31 @@
-export class kkk {
-  constructor(){
-  }
+import {HttpClient, json} from 'aurelia-fetch-client'
+
+export class arvutuseVorm{
+
+	reviewData = {}
+	reviewList = []
+
+	activate() {
+		let client = new HttpClient();
+		client.fetch('http://localhost:8080/reviews')
+			.then(response => response.json())
+			.then(reviews => this.reviewList = reviews);
+			console.log("Get method ex.!")
+	}
+
+	addReview() {
+	  this.activate();
+		let client = new HttpClient();
+
+		client.fetch('http://localhost:8080/reviews/add', {
+			'method': "POST",
+			'body': json(this.reviewData)
+		})
+			.then(response => response.json())
+			.then(data => {
+				console.log("Server saatis " + data.box);
+		});
+
+		console.log("Method executed!")
+	}
 }
