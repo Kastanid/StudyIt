828a2833d85b86feda379bdae909a4224fed97e2
Laura <laurakallas7@gmail.com>
2018-04-04 10:52:10 +0300

    Java klassid õiged ja toimivad + andmebaasi seadistus

--- backend/src/main/java/hello/Review.java
+++ backend/src/main/java/hello/Review.java
@@ -1,20 +1,26 @@
 package hello;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Table;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Getter
+@Setter
 public class Review {
-
-    private final String title;
-    private final String content;
-
-    public Review(String title, String content) {
-        this.title = title;
-        this.content = content;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getContent() {
-        return content;
-    }
+    @Id
+    @GeneratedValue
+    int id;
+    String box;
+    String rating;
+    int score;
+    String reviewBox;
 }
